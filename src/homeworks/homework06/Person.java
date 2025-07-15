69bab85953bcaacb753b8de3e60f9262990521e2
aztek17 <aztek1717@gmail.com>
2025-07-15 23:08:41 +0400

    Добавлены вспомогательные методы и проверки

--- src/homeworks/homework06/Person.java
+++ src/homeworks/homework06/Person.java
@@ -1,10 +1,18 @@
 package homeworks.homework06;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Person {
     private String name;
     private int amountMoney;
-    private Product[] products;
+    private ArrayList<Product> products;
 
+    public Person(String name, int amountMoney, ArrayList<Product> products) {
+        setName(name);
+        this.amountMoney = amountMoney;
+    }
 
     public String getName() {
         return name;
@@ -13,6 +21,8 @@ public class Person {
     public void setName(String name) {
         if (!name.isEmpty()) {
             this.name = name;
+        } else {
+            System.out.println("Имя не может быть пустым");
         }
     }
 
@@ -23,14 +33,47 @@ public class Person {
     public void setAmountMoney(int amountMoney) {
         if (amountMoney > 0) {
             this.amountMoney = amountMoney;
+        }  else {
+            System.out.println("Деньги не могут быть отрицательными");
         }
     }
 
-    public Product[] getProducts() {
+    public List<Product> getProducts() {
         return products;
     }
 
-    public void setProducts(Product[] products) {
-        this.products = products;
+    public void addProduct(Product product) {
+        if (getAmountMoney() >= product.getProductPrice()) {
+            this.products.add(product);
+            setAmountMoney(getAmountMoney() - product.getProductPrice());
+            System.out.println(getName() + " купил " + product.getProductName());
+        } else {
+            System.out.println(getName() + " не может позволить себе " + product.getProductName());
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+        return amountMoney == person.amountMoney && Objects.equals(name, person.name) && Objects.equals(products, person.products);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(name);
+        result = 31 * result + amountMoney;
+        result = 31 * result + Objects.hashCode(products);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", amountMoney=" + amountMoney +
+                ", products=" + products +
+                '}';
     }
 }
--- src/homeworks/homework06/Product.java
+++ src/homeworks/homework06/Product.java
@@ -1,8 +1,15 @@
 package homeworks.homework06;
 
+import java.util.Objects;
+
 public class Product {
     private String productName;
     private int productPrice;
+
+    public Product(String productName, int productPrice) {
+        setProductName(productName);
+        setProductPrice(productPrice);
+    }
 
     public String getProductName() {
         return productName;
@@ -11,6 +18,8 @@ public class Product {
     public void setProductName(String productName) {
         if (!productName.isEmpty()) {
             this.productName = productName;
+        } else {
+            System.out.println("Название товара не может быть пустым");
         }
     }
 
@@ -19,8 +28,33 @@ public class Product {
     }
 
     public void setProductPrice(int productPrice) {
-        if (productPrice > 0) {
+        if (productPrice >= 0) {
             this.productPrice = productPrice;
+        } else {
+            System.out.println("Цена товара не может быть меньше нуля");
         }
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productName='" + productName + '\'' +
+                ", productPrice=" + productPrice +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+        return productPrice == product.productPrice && Objects.equals(productName, product.productName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(productName);
+        result = 31 * result + productPrice;
+        return result;
     }
 }
