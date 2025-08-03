92a6e6267edc5a262108d73353c39da1ff968113
aztek17 <aztek1717@gmail.com>
2025-08-04 00:31:57 +0400

    Переопределение логики покупки для взрослых

--- src/homeworks/homework07/Adult.java
+++ src/homeworks/homework07/Adult.java
@@ -1,8 +1,35 @@
 package homeworks.homework07;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Adult extends Person {
+
+    private final ArrayList<Product> products = new ArrayList<>();
+    private boolean buyCredit;
 
     public Adult(String name, int amountMoney, int age) {
         super(name, amountMoney, age);
+        this.buyCredit = new Random().nextBoolean();
+    }
+
+    public void addProduct(Product product) {
+        if (getAmountMoney() >= product.getProductPrice()) {
+            this.products.add(product);
+            setAmountMoney(getAmountMoney() - product.getProductPrice());
+            System.out.println(getName() + " купил " + product.getProductName());
+        } else if (getAmountMoney() < product.getProductPrice() && isBuyCredit()) {
+            System.out.println(getName() + " купил товар " + product.getProductName() + " в кредит");
+        } else {
+            System.out.println(getName() + " не может позволить себе " + product.getProductName());
+        }
+    }
+
+    public boolean isBuyCredit() {
+        return buyCredit;
+    }
+
+    public void setBuyCredit(boolean buyCredit) {
+        this.buyCredit = buyCredit;
     }
 }
