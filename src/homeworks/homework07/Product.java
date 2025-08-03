265c0cfa5172cbd7b3e2b11f806afcd55e6a6b6e
aztek17 <aztek1717@gmail.com>
2025-08-03 12:10:50 +0400

    Base homework 7. Add discount products

---
+++ src/homeworks/homework07/Product.java
@@ -0,0 +1,61 @@
+package homeworks.homework07;
+
+import java.util.Objects;
+
+public class Product {
+    private String productName;
+    private int productPrice;
+
+    public Product(String productName, int productPrice) {
+        setProductName(productName);
+        setProductPrice(productPrice);
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        if (!productName.isEmpty()) {
+            if (productName.length() < 3 || productName.matches("\\d+")) {
+                System.out.println("Недопустимое имя продукта");
+                return;
+            }
+            this.productName = productName;
+        } else {
+            System.out.println("Название товара не может быть пустым");
+        }
+    }
+
+    public int getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(int productPrice) {
+        if (productPrice > 0) {
+            this.productPrice = productPrice;
+        } else {
+            System.out.println("Недопустимая стоимость продукта");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return (productName == null ? "" : productName);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+        return productPrice == product.productPrice && Objects.equals(productName, product.productName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(productName);
+        result = 31 * result + productPrice;
+        return result;
+    }
+}
