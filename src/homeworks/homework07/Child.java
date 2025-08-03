2b759bd0cec1fd6edf41ddc02d27011724f4400b
aztek17 <aztek1717@gmail.com>
2025-08-04 00:31:40 +0400

    Переопределение логики покупки для детей

--- src/homeworks/homework07/Child.java
+++ src/homeworks/homework07/Child.java
@@ -5,4 +5,15 @@ public class Child extends Person {
     public Child(String name, int amountMoney, int age) {
         super(name, amountMoney, age);
     }
+
+    @Override
+    public void addProduct(Product product) {
+        if (getAge() < 6) {
+            System.out.println("Ребенок не может покупать продукты");
+        } else if (!product.isAccessibleToChild()) {
+            System.out.println("Этот товар не разрешен к продаже ребенку");
+        } else {
+            super.addProduct(product);
+        }
+    }
 }
