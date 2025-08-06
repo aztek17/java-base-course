82f7613ab82c3891fda244a0c1a5472c458fe163
aztek17 <aztek1717@gmail.com>
2025-08-06 20:03:29 +0400

    Исправлена ошибка логики вывода

--- src/homeworks/homework08/Adult.java
+++ src/homeworks/homework08/Adult.java
@@ -21,10 +21,9 @@ public class Adult extends Person {
         } else if (getAmountMoney() < product.getProductPrice() && isBuyCredit()) {
             getProducts().add(product);
             setAmountMoney(getAmountMoney() - product.getProductPrice());
-            App.writeToFile(getName() + " купил " + product.getProductName());
+            App.writeToFile(getName() + " купил " + product.getProductName() + " в кредит");
         } else {
             App.writeToFile(getName() + " не может позволить себе " + product.getProductName());
-            setAmountMoney(getAmountMoney() - product.getProductPrice());
         }
     }
 
