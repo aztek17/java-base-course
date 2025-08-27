037bbc88117283e05f104dd705b05ce89495c6bd
aztek17 <aztek1717@gmail.com>
2025-08-27 17:57:21 +0400

    Добавлен ввод из консоли

--- src/homeworks/homework12/task2/Main.java
+++ src/homeworks/homework12/task2/Main.java
@@ -1,10 +1,16 @@
 package homeworks.homework12.task2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String login = "berserk_911";
-        String password = "kresreb771_177";
-        String confirmPassword = "kresreb771_177";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите логин");
+        String login = scanner.nextLine();
+        System.out.println("Введите пароль");
+        String password = scanner.nextLine();
+        System.out.println("Введите подтверждение пароля");
+        String confirmPassword = scanner.nextLine();
         boolean valid;
 
         valid = User.validateAuthData(login, password, confirmPassword);
