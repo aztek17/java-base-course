e2c9e1501526ef83c2a115f430cc44f946f852c1
aztek17 <aztek1717@gmail.com>
2025-11-16 14:51:22 +0400

    homework18. Update models

--- src/homeworks/homework18/src/main/java/ru/base/shop/model/Customer.java
+++ src/homeworks/homework18/src/main/java/ru/base/shop/model/Customer.java
@@ -1,4 +1,22 @@
 package ru.base.shop.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "customer")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "customer_name")
+    private String customerName;
+
 }
--- src/homeworks/homework18/src/main/java/ru/base/shop/model/Orders.java
+++ src/homeworks/homework18/src/main/java/ru/base/shop/model/Orders.java
@@ -1,4 +1,28 @@
 package ru.base.shop.model;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "orders")
 public class Orders {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne()
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @Column(name = "order_date")
+    private Date orderDate;
+
+    @Column(name = "count_order")
+    private Integer countOrder;
+
+    @Column(name = "discount")
+    private Integer discount;
+
 }
