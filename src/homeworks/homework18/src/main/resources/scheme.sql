af4706f0dd47ff1064189a4d21fa402bf5fea09f
aztek17 <aztek1717@gmail.com>
2025-11-16 23:43:33 +0400

    homework18. Manual SQL scripts

--- src/homeworks/homework18/src/main/resources/scheme.sql
+++ src/homeworks/homework18/src/main/resources/scheme.sql
@@ -20,3 +20,18 @@ create table if not exists shop.orders
 
     foreign key (customer_id) references shop.customer (id)
 );
+
+-- добавление информации
+insert into shop.customer(customer_name)
+values ('Петров Николай'),
+('Спиридонов Дмитрий'),
+('Марьина Дарья');
+
+insert into shop.orders(customer_id, order_date, count_order, discount)
+values ('1', '2025-06-15', 1, 0),
+('2', '2025-12-07', 1, 10),
+('3', '2024-03-18', 1, 5),
+('1', '2025-12-20', 2, 15),
+('1', '2025-12-21', 3, 7);
+
+commit;
