a03f18833bd371f2e2437048461f810d4aa93d2b
aztek17 <aztek1717@gmail.com>
2025-11-16 23:45:23 +0400

    homework18. Fix run app

--- src/homeworks/homework18/src/main/java/ru/base/shop/ShopApp.java
+++ src/homeworks/homework18/src/main/java/ru/base/shop/ShopApp.java
@@ -1,11 +1,32 @@
 package ru.base.shop;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.base.shop.repository.OrdersRepository;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
-public class ShopApp {
+@RequiredArgsConstructor
+public class ShopApp implements CommandLineRunner {
+
+    private final OrdersRepository ordersRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(ShopApp.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        System.out.println("Заказ с ID 3: " + ordersRepository.findById(3));
+        System.out.println("Заказ с датой 2025-12-07 : " + ordersRepository.findByOrderDate(LocalDate.of(2025, 12, 7)));
+        System.out.println("Список все заказов до удаления по ID: " + ordersRepository.findAll());
+        ordersRepository.deleteById(3);
+        System.out.println("Список все заказов после удаления по ID: " + ordersRepository.findAll());
+        ordersRepository.deleteAll();
+        System.out.println("Список заказов после отчистки: " + ordersRepository.findAll());
+
     }
 }
