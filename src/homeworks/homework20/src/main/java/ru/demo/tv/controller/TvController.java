0acc9d83346b029f44be46e8c4abba40c4a25d8e
aztek17 <aztek1717@gmail.com>
2025-11-23 23:51:31 +0400

    homework20. Implemented controller tv

--- src/homeworks/homework20/src/main/java/ru/demo/tv/controller/TvController.java
+++ src/homeworks/homework20/src/main/java/ru/demo/tv/controller/TvController.java
@@ -1,12 +1,60 @@
 package ru.demo.tv.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.demo.tv.dto.TvDto;
 import ru.demo.tv.service.TvService;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("api/")
 public class TvController {
 
     private final TvService service;
+
+    @PostMapping("/tv")
+    public ResponseEntity<TvDto> createTv(@RequestBody TvDto dto) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(service.createTv(dto));
+    }
+
+    @GetMapping("/tv")
+    public ResponseEntity<List<TvDto>> getTvs() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(service.getAllTvs());
+    }
+
+    @GetMapping("/tv/{id}")
+    public ResponseEntity<TvDto> getTvById(@PathVariable("id") Long id) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(service.getTvById(id));
+    }
+
+    @GetMapping("/tv/find")
+    public ResponseEntity<List<TvDto>> getTvsByBrand(@RequestParam(name = "brand") String brand) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(service.getTvByBrand(brand));
+    }
+
+    @PutMapping("/tv/{id}")
+    public ResponseEntity<TvDto> updateTv(@PathVariable("id") Long id, @RequestBody TvDto dto) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(service.updateTv(id, dto));
+    }
+
+    @DeleteMapping("/tv/{id}")
+    public ResponseEntity<TvDto> deleteTv(@PathVariable("id") Long id) {
+        service.deleteTv(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
 }
