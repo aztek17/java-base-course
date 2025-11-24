ddf68f94ff50fe63a3691c5b685ae06ccc6a1ff7
aztek17 <aztek1717@gmail.com>
2025-11-25 00:46:54 +0400

    homework21. Copy app

---
+++ src/homeworks/homework21/src/main/java/ru/demo/tv/model/Tv.java
@@ -0,0 +1,35 @@
+package ru.demo.tv.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Setter
+@Getter
+@Entity
+@Builder
+@ToString
+@Table(name = "tv", schema = "televisions")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tv {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "brand")
+    private String brand;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "price")
+    private Integer price;
+
+    @Column(name = "voice_assistant")
+    private boolean voiceAssistant;
+
+    @Column(name = "screen_size")
+    private Integer screenSize;
+
+}
