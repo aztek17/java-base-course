5a813c2cc6f85f1d2872f1f84ca4c155ce555922
aztek17 <aztek1717@gmail.com>
2025-12-02 16:31:04 +0400

    attestation3. Add soft delete and filter exclude deleted

--- src/attestations/attestation3/src/main/java/ru/test/tireservice/repository/OrderRepository.java
+++ src/attestations/attestation3/src/main/java/ru/test/tireservice/repository/OrderRepository.java
@@ -1,14 +1,28 @@
 package ru.test.tireservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.test.tireservice.model.Order;
 import ru.test.tireservice.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    @Query("SELECT o FROM Order o WHERE o.isDeleted = false")
+    List<Order> findAllActive();
+
+    @Query("SELECT o FROM Order o WHERE o.id = :id AND o.isDeleted = false")
+    Optional<Order> findActiveById(@Param("id") Long id);
+
+    @Modifying
+    @Query("UPDATE Order o SET o.isDeleted = true WHERE o.id = :id")
+    void softDelete(@Param("id") Long id);
+
     List<Order> findByCustomer(User customer);
 
     List<Order> findByStatus(Order.OrderStatus status);
